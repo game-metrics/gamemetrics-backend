be1223386a12ec823cc21eefaab2a3fcfdaaff96
harrisbang2 <harrisbang2@gmail.com>
2024-06-19 21:41:08 +0900

    fix(user) : apache.catalina.User bug fix -> personal made user entity

--- src/main/java/com/gamemetricbackend/domain/user/service/UserService.java
+++ src/main/java/com/gamemetricbackend/domain/user/service/UserService.java
@@ -1,9 +1,9 @@
 package com.gamemetricbackend.domain.user.service;
 
+import com.gamemetricbackend.domain.user.entitiy.User;
 import com.gamemetricbackend.domain.user.repository.UserRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
