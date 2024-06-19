bf3203a0f02928d0695543bb632c16d988c20caf
harrisbang2 <harrisbang2@gmail.com>
2024-06-19 20:48:58 +0900

    init commit

---
+++ src/test/java/com/gamemetricbackend/GamemetricBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamemetricbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamemetricBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
