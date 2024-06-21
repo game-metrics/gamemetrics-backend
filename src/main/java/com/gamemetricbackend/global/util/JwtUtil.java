4f7d3eb632fea40608103cfad78a6bd818a5b8df
harrisbang2 <harrisbang2@gmail.com>
2024-06-21 20:06:35 +0900

    feat(global) : utils init value

--- src/main/java/com/gamemetricbackend/global/util/JwtUtil.java
+++ src/main/java/com/gamemetricbackend/global/util/JwtUtil.java
@@ -1,29 +1,35 @@
 package com.gamemetricbackend.global.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.gamemetricbackend.global.entity.RefreshToken;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
-import jakarta.servlet.http.HttpServletRequest;
 import java.security.Key;
-import java.util.Base64;
-import java.util.Date;
-import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 @Slf4j(topic = "JwtUtil")
 @Component
 @RequiredArgsConstructor
 public class JwtUtil {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    // 사용자 권한 값의 KEY
+    public static final String AUTHORIZATION_KEY = "auth";
+    // Token 식별자
+    public static final String BEARER_PREFIX = "Bearer ";
+    // 토큰 만료시간
+    private final long TOKEN_TIME = 24 * 60 * 60 * 1000L; // 하루
+
+    // jwt 알고리즘 키
+    @Value("${jwt.secret.key}") // Base64 Encode 한 SecretKey
+    private String secretKey;
+
+    // JWT 알고리즘
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+
+
+
 
 
 }
