0d8c78ca0494b9af46c3667a2ac2ff118cd9ad13
harrisbang2 <harrisbang2@gmail.com>
2024-06-27 23:21:15 +0900

    feat(global) : Jwt util, create token , claim token, createAuthentication functions made

--- src/main/java/com/gamemetricbackend/global/util/JwtUtil.java
+++ src/main/java/com/gamemetricbackend/global/util/JwtUtil.java
@@ -1,11 +1,26 @@
 package com.gamemetricbackend.global.util;
 
+import com.gamemetricbackend.domain.user.entitiy.UserRoleEnum;
+import com.gamemetricbackend.global.impl.UserDetailsImpl;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import java.security.Key;
+import java.util.Base64;
+import java.util.Collection;
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Slf4j(topic = "JwtUtil")
 @Component
@@ -13,23 +28,72 @@ import org.springframework.stereotype.Component;
 public class JwtUtil {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
-    // 사용자 권한 값의 KEY
     public static final String AUTHORIZATION_KEY = "auth";
-    // Token 식별자
     public static final String BEARER_PREFIX = "Bearer ";
-    // 토큰 만료시간
-    private final long TOKEN_TIME = 24 * 60 * 60 * 1000L; // 하루
+    private final long ACCESS_TOKEN_TIME = 24 * 60 * 60 * 1000L; // 하루
 
-    // jwt 알고리즘 키
+    public static final String USER_ID = "userId";
+
     @Value("${jwt.secret.key}") // Base64 Encode 한 SecretKey
     private String secretKey;
-
-    // JWT 알고리즘
+    private Key key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
 
+    public String createToken(
+        final Long memberId,
+        final UserRoleEnum role
+    ) {
+        Date date = new Date();
 
+        return BEARER_PREFIX +
+            Jwts.builder()
+                .setSubject(USER_ID)
+                .claim(USER_ID, memberId)
+                .claim(AUTHORIZATION_KEY, role)
+                .setExpiration(new Date(date.getTime() + ACCESS_TOKEN_TIME))
+                .setIssuedAt(date)
+                .signWith(key, signatureAlgorithm)
+                .compact();
+    }
 
+    public String getJwtFromHeader(
+        final HttpServletRequest request
+    ) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 
+    public Claims claimUserInfoFromToken(
+        final String token
+    ) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token)
+            .getBody();
+    }
 
+    private UserDetails getUserDetailsFromToken(
+        final Claims info
+    ) {
+        Long memberId = ((Number) info.get(USER_ID)).longValue();
+        UserRoleEnum userRole = UserRoleEnum.valueOf((String) info.get(AUTHORIZATION_KEY));
+        return new UserDetailsImpl(memberId, userRole);
+    }
+
+    public Authentication createAuthentication(
+        final String token
+    ) {
+        Claims info = claimUserInfoFromToken(token);
+        UserDetails userDetails = getUserDetailsFromToken(info);
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+        return new UsernamePasswordAuthenticationToken(
+            userDetails, null, authorities);
+    }
 }
