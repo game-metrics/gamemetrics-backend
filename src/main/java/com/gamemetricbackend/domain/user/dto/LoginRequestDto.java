c4d799b78a468757caeb6e8bec2b4a64f16b55d4
harrisbang2 <harrisbang2@gmail.com>
2024-07-02 21:13:15 +0900

    feat(user) : LoginRequestDto init

---
+++ src/main/java/com/gamemetricbackend/domain/user/dto/LoginRequestDto.java
@@ -0,0 +1,12 @@
+package com.gamemetricbackend.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+    String username;
+    String password;
+}
