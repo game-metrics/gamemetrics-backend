27181dbd485ca8007ab83612fe7632e507dbaacd
harrisbang2 <harrisbang2@gmail.com>
2024-07-05 13:47:33 +0900

    feat(user) : UserRepositoryQuery, UserRepositoryQueryImpl 1.0.1 created

---
+++ src/main/java/com/gamemetricbackend/domain/user/repository/UserRepositoryQuery.java
@@ -0,0 +1,5 @@
+package com.gamemetricbackend.domain.user.repository;
+
+public interface UserRepositoryQuery {
+
+}
---
+++ src/main/java/com/gamemetricbackend/domain/user/repository/UserRepositoryQueryImpl.java
@@ -0,0 +1,5 @@
+package com.gamemetricbackend.domain.user.repository;
+
+public class UserRepositoryQueryImpl implements UserRepositoryQuery{
+
+}
