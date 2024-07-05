5b2b5a0ef9b7ee6e41ad3e96ce6442c2e90950b7
harrisbang2 <harrisbang2@gmail.com>
2024-07-05 16:33:20 +0900

    feat(user) : user api request changed to plurals

--- src/main/java/com/gamemetricbackend/domain/user/controller/UserController.java
+++ src/main/java/com/gamemetricbackend/domain/user/controller/UserController.java
@@ -3,22 +3,24 @@ package com.gamemetricbackend.domain.user.controller;
 import com.gamemetricbackend.domain.user.dto.SignupRequestDto;
 import com.gamemetricbackend.domain.user.service.UserService;
 import com.gamemetricbackend.global.aop.dto.CommonResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
     @PostMapping()
-    public ResponseEntity<Void> signup(SignupRequestDto requestDto){
+    public ResponseEntity<Void> signup(@Valid @RequestBody SignupRequestDto requestDto){
         userService.signup(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
