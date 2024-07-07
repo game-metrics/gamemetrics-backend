45917a50336c72f20f312d435c61e12d194bdcac
harrisbang2 <harrisbang2@gmail.com>
2024-07-07 20:36:51 +0900

    feat(user) : Password Update(patch) function created 1.0.0

--- src/main/java/com/gamemetricbackend/domain/user/service/UserService.java
+++ src/main/java/com/gamemetricbackend/domain/user/service/UserService.java
@@ -1,11 +1,16 @@
 package com.gamemetricbackend.domain.user.service;
 
 import com.gamemetricbackend.domain.user.dto.SignupRequestDto;
+import com.gamemetricbackend.domain.user.dto.UpdatePasswordRequestDto;
 import com.gamemetricbackend.domain.user.entitiy.User;
+import com.gamemetricbackend.global.exception.NoSuchUserException;
 import java.util.Optional;
 
 public interface UserService {
     public Optional<User> findById(Long id);
 
     void signup(SignupRequestDto requestDto);
+
+    boolean UpdatePassword(Long userId, UpdatePasswordRequestDto updatePasswordRequestDto)
+        throws NoSuchUserException;
 }
