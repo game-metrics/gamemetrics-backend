3d5988e7a4e8dc599b9645c845f4e16a952a4ae1
harrisbang2 <harrisbang2@gmail.com>
2024-07-12 23:55:34 +0900

    feat(broadcast) : broadcast search function 1.0.1

--- src/main/java/com/gamemetricbackend/domain/broadcast/repository/BroadcastRepository.java
+++ src/main/java/com/gamemetricbackend/domain/broadcast/repository/BroadcastRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BroadcastRepository extends JpaRepository<Broadcast,Long>,BroadcastRepositoryQuery {
     Optional<Broadcast> findById(Long id);
+
+    Broadcast findByTitle(String title);
 }
