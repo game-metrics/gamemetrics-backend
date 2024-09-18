60f5845e0f06962d0bd2cd03a38dc7fff5ce04d3
harrisbang2 <harrisbang2@gmail.com>
2024-09-18 23:44:40 +0900

    feat(dib) : dib does no longer stores a class(entity) of user as OneToMany, it now only requires the name of the Streamer and indexing added

--- src/main/java/com/gamemetricbackend/domain/dib/entity/Dib.java
+++ src/main/java/com/gamemetricbackend/domain/dib/entity/Dib.java
@@ -7,22 +7,25 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @RequiredArgsConstructor
+@Table(indexes = {@Index(name = "dib_index",columnList = "followerId,streamerName")})
 public class Dib {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "followerId", nullable = false)
     private Long followerId;
 
     // todo : need a better way to get streamer name, using less access to the DB and lesser query using dsl.
-    @Column(nullable = false)
+    @Column( name = "streamerName", nullable = false)
     private String streamerName;
 }
