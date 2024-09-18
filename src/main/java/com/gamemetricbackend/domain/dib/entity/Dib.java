d99dc793091ba975df41f3d51715ba404c5b9e42
harrisbang2 <harrisbang2@gmail.com>
2024-09-19 00:02:17 +0900

    feat(dib) : dib sub status has added

--- src/main/java/com/gamemetricbackend/domain/dib/entity/Dib.java
+++ src/main/java/com/gamemetricbackend/domain/dib/entity/Dib.java
@@ -1,19 +1,17 @@
 package com.gamemetricbackend.domain.dib.entity;
 
-import com.gamemetricbackend.domain.user.entitiy.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.List;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Entity
+@Getter
 @RequiredArgsConstructor
 @Table(indexes = {@Index(name = "dib_index",columnList = "follower_Id,streamer_name")})
 public class Dib {
@@ -26,6 +24,20 @@ public class Dib {
     private Long followerId;
 
     // todo : need a better way to get streamer name, using less access to the DB and lesser query using dsl.
-    @Column( name = "streamer_name", nullable = false)
+    @Column(name = "streamer_name", nullable = false)
     private String streamerName;
+
+    @Column(name = "dib_status")
+    private Boolean status;
+
+    public Dib(Long userid, String streamerName) {
+        this.followerId=userid;
+        this.streamerName=streamerName;
+        this.status = false;
+    }
+
+    public void updateStatus(){
+        if(status == true) status = false;
+        else status = true;
+    }
 }
