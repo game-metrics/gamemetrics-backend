3c46fa9e6e8d5f9f93a9bd0e5469ea78cb1dd42f
harrisbang2 <harrisbang2@gmail.com>
2024-09-18 23:47:42 +0900

    feat(dib) : upateDib contoller function init

--- src/main/java/com/gamemetricbackend/domain/dib/controller/DibController.java
+++ src/main/java/com/gamemetricbackend/domain/dib/controller/DibController.java
@@ -1,8 +1,15 @@
 package com.gamemetricbackend.domain.dib.controller;
 
 import com.gamemetricbackend.domain.dib.service.DibService;
+import com.gamemetricbackend.domain.user.service.UserService;
+import com.gamemetricbackend.domain.user.service.UserServiceImpl;
+import com.gamemetricbackend.global.impl.UserDetailsImpl;
+import java.nio.file.attribute.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/dib")
 public class DibController {
     private DibService dibService;
+
+    @GetMapping
+    public Boolean upateDib(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable(value = "streamerName") String StreamerName){
+        return dibService.upateDib(userDetails.getId(),StreamerName);
+    }
 
 }
