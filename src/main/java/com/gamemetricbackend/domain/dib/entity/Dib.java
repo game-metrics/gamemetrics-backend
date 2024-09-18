7f5bcf856335fabaa789868efc833063faa13409
harrisbang2 <harrisbang2@gmail.com>
2024-09-18 23:45:01 +0900

    feat(dib) : name changing for table columns in dib table

--- src/main/java/com/gamemetricbackend/domain/dib/entity/Dib.java
+++ src/main/java/com/gamemetricbackend/domain/dib/entity/Dib.java
@@ -15,17 +15,17 @@ import lombok.RequiredArgsConstructor;
 
 @Entity
 @RequiredArgsConstructor
-@Table(indexes = {@Index(name = "dib_index",columnList = "followerId,streamerName")})
+@Table(indexes = {@Index(name = "dib_index",columnList = "follower_Id,streamer_name")})
 public class Dib {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(name = "followerId", nullable = false)
+    @Column(name = "follower_Id", nullable = false)
     private Long followerId;
 
     // todo : need a better way to get streamer name, using less access to the DB and lesser query using dsl.
-    @Column( name = "streamerName", nullable = false)
+    @Column( name = "streamer_name", nullable = false)
     private String streamerName;
 }
