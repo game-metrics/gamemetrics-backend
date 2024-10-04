3a9a14a06880323edf2447ff50da4ef5d3f75dae
harrisbang2 <harrisbang2@gmail.com>
2024-10-04 20:56:28 +0900

    feat(broadcast) : paging Serialization mode added VIA_DTO for better the view

--- src/main/java/com/gamemetricbackend/GamemetricBackendApplication.java
+++ src/main/java/com/gamemetricbackend/GamemetricBackendApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class GamemetricBackendApplication {
 
     public static void main(String[] args) {
