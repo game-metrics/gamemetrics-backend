882bd9d4defc7fe809a2d7d885b87867a120ca15
harrisbang2 <harrisbang2@gmail.com>
2024-10-09 08:38:49 +0900

    feat(aws) : s3 controller ,service init

---
+++ src/main/java/com/gamemetricbackend/global/aws/s3/service/AwsS3Service.java
@@ -0,0 +1,11 @@
+package com.gamemetricbackend.global.aws.s3.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AwsS3Service {
+
+}
+
