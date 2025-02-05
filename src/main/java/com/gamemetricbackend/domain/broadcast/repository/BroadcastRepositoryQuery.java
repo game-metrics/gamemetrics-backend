5b988524c360ce510eadb7b3dfe3758c3736974b
harrisbang2 <harrisbang2@gmail.com>
2025-02-05 17:27:18 +0900

    feat(broadcast) : dto 변경, response 에 id 추가

--- src/main/java/com/gamemetricbackend/domain/broadcast/repository/BroadcastRepositoryQuery.java
+++ src/main/java/com/gamemetricbackend/domain/broadcast/repository/BroadcastRepositoryQuery.java
@@ -1,12 +1,13 @@
 package com.gamemetricbackend.domain.broadcast.repository;
 
+import com.gamemetricbackend.domain.broadcast.dto.BroadCastResponseDto;
 import com.gamemetricbackend.domain.broadcast.dto.BroadcastCreationDto;
 import com.gamemetricbackend.domain.broadcast.entitiy.Broadcast;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface BroadcastRepositoryQuery {
-    Page<BroadcastCreationDto> findByTitle(String title, Pageable pageable);
+    Page<BroadCastResponseDto> findByTitle(String title, Pageable pageable);
 
-    Page<BroadcastCreationDto> getBroadcatePage(Pageable pageable);
+    Page<BroadCastResponseDto> getBroadcatePage(Pageable pageable);
 }
