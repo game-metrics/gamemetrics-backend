a6395a3209fb5718a6c7a129dca5676b29446b4c
harrisbang2 <harrisbang2@gmail.com>
2025-02-13 17:47:54 +0900

    feat(user) :  유저 개인 정보 수집 추가, 유저 검색 추가 // TODO 검색 알고리즘을 추가 할가합니다 (edit distance 알고리즘)

--- src/main/java/com/gamemetricbackend/domain/user/repository/UserRepositoryQuery.java
+++ src/main/java/com/gamemetricbackend/domain/user/repository/UserRepositoryQuery.java
@@ -1,10 +1,15 @@
 package com.gamemetricbackend.domain.user.repository;
 
+import com.gamemetricbackend.domain.user.dto.response.UserInfoResponseDto;
 import com.gamemetricbackend.domain.user.entitiy.User;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UserRepositoryQuery {
     Optional<User> findPasswordById(Long id);
 
     Optional<User> findByEmail(String getEmail);
+
+    Page<UserInfoResponseDto> SearchUsersByNickName(String nickName, Pageable pageable);
 }
