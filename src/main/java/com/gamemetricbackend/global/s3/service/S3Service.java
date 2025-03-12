5de6faec5a531f49b93736c4d3822344338c4b3b
harrisbang2 <harrisbang2@gmail.com>
2025-03-12 17:42:30 +0900

    feat/S3 : S3 controller , service init

--- src/main/java/com/gamemetricbackend/global/s3/service/S3Service.java
+++ src/main/java/com/gamemetricbackend/global/s3/service/S3Service.java
@@ -4,15 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.UUID;
 
 @Service
