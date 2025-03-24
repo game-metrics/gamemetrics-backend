e4fa0b6a9e9e8ef2f8c8d0ab12da6ca63a0c11a3
harrisbang2 <harrisbang2@gmail.com>
2025-03-24 19:36:50 +0900

    feat/follow : getting my following list function

---
+++ src/main/java/com/gamemetricbackend/domain/follow/dto/FollowResponseDto.java
@@ -0,0 +1,12 @@
+package com.gamemetricbackend.domain.follow.dto;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class FollowResponseDto {
+    String streamerName;
+}
